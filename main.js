476e9585407f7de81d6ca922804684f3395dca3f
Stalker-Pro <andreit007@mail.ru>
2024-10-09 22:55:56 +0700

    ok man

--- main.js
+++ main.js
@@ -1,41 +1,95 @@
 'use strict'; ///строгий режим в js
 
-const button1 = document.querySelector('.button--1');
-const button2 = document.querySelector('.button--2');
-const button3 = document.querySelector('.button--3');
-const dog = document.querySelector('.dog');
-const cat = document.querySelector('.cat');
-const imageDog = document.querySelector('.image-dog');
-const imageCat = document.querySelector('.image-cat');
+const container = document.querySelector('.place'); /// input.value чтобы получить значения с input
+const input = document.querySelector('.input');
+
+const triangleBtn = document.querySelector('.triangle');
+const squareBtn = document.querySelector('.square');
+const circleBtn = document.querySelector('.circle');
 
 
-button1.addEventListener('click', () => {
-  cat.classList.remove('active');
-  dog.classList.remove('active');
-  imageDog.classList.remove('active');
-  imageCat.classList.remove('active');
 
-  imageCat.classList.remove('none');
-  cat.classList.remove('none');
-  dog.classList.remove('right');
-});
+const drawSquare = `
+  <div class="addSquare"></div>
+`;
 
-button2.addEventListener('click', () => {
-  cat.classList.add('active');
-  dog.classList.add('active');
-  imageDog.classList.add('active');
-  imageCat.classList.add('active');
+const drawTriangle = `
+  <div class="addTriangle"></div>
+`;
 
-  imageCat.classList.remove('none');
-  cat.classList.remove('none');
-  dog.classList.remove('right');
+const drawCircle = `
+  <div class="addCircle"></div>
+`;
+
+triangleBtn.addEventListener('click', (e) => {
+  const count = Number(input.value);
+
+  if (!count) return;
+
+  for (let i = 0; i < count; i++) {
+    container.insertAdjacentHTML("beforeend",drawTriangle);
+  }
 })
 
-button3.addEventListener('click', () => {
-  cat.classList.add('none');
 
-  dog.classList.add('right');
+squareBtn.addEventListener('click', (e) => {
+  const count = Number(input.value);
 
-  imageDog.classList.add('active');
-  imageCat.classList.add('none');
+  if (!count) return;
+
+  for (let i = 0; i < count; i++) {
+    container.insertAdjacentHTML("beforeend",drawSquare);
+  }
 })
+
+
+circleBtn.addEventListener('click', (e) => {
+  const count = Number(input.value);
+
+  if (!count) return;
+
+  for (let i = 0; i < count; i++) {
+    container.insertAdjacentHTML("beforeend",drawCircle);
+  }
+})
+
+
+// const button1 = document.querySelector('.button--1');
+// const button2 = document.querySelector('.button--2');
+// const button3 = document.querySelector('.button--3');
+// const dog = document.querySelector('.dog');
+// const cat = document.querySelector('.cat');
+// const imageDog = document.querySelector('.image-dog');
+// const imageCat = document.querySelector('.image-cat');
+
+
+// button1.addEventListener('click', () => {
+//   cat.classList.remove('active');
+//   dog.classList.remove('active');
+//   imageDog.classList.remove('active');
+//   imageCat.classList.remove('active');
+
+//   imageCat.classList.remove('none');
+//   cat.classList.remove('none');
+//   dog.classList.remove('right');
+// });
+
+// button2.addEventListener('click', () => {
+//   cat.classList.add('active');
+//   dog.classList.add('active');
+//   imageDog.classList.add('active');
+//   imageCat.classList.add('active');
+
+//   imageCat.classList.remove('none');
+//   cat.classList.remove('none');
+//   dog.classList.remove('right');
+// })
+
+// button3.addEventListener('click', () => {
+//   cat.classList.add('none');
+
+//   dog.classList.add('right');
+
+//   imageDog.classList.add('active');
+//   imageCat.classList.add('none');
+// })
